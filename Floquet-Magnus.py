59229219b1331b8a1401db548b42aca8a75704ac
Callum Brown <callumbrown101@gmal.com>
2026-06-09 15:49:54 +0200

    Added micromotion deviation plot and quasienergy plot

--- Floquet-Magnus.py
+++ Floquet-Magnus.py
@@ -22,7 +22,7 @@ def main():
 
     app.mainloop()
 
-def H_func(t, omega):
+def H_func(t, omega, A):
     Sx = (1/np.sqrt(2)) * np.array([[0, 1, 0],
                                     [1, 0, 1],
                                     [0, 1, 0]], dtype=complex)
@@ -30,15 +30,15 @@ def H_func(t, omega):
                    [0, 0, 0],
                    [0, 0,-1]], dtype=complex)
     # Static Sz + oscillating Sx — these don't commute
-    return Sz + 0.5 * np.sin(omega*t) * Sx
+    return Sz + A * np.sin(omega*t) * Sx
 
 
-def floquet_magnus_expansion(H_func, t_grid, omega, order):
-    dim = H_func(0, omega).shape[0]
+def floquet_magnus_expansion(H_func, t_grid, omega, A, order):
+    dim = H_func(0, omega, A).shape[0]
     B = [float(bernoulli(n)/factorial(n)) for n in range(order)]
     H = np.zeros((len(t_grid), dim, dim), dtype=complex)
     for i, t in enumerate(t_grid):
-        H[i] = H_func(t, omega)
+        H[i] = H_func(t, omega, A)
     F = np.empty(order, dtype=object)
     G = np.empty(order, dtype=object)
     W = np.empty((order, order), dtype=object)
@@ -80,11 +80,11 @@ def floquet_magnus_expansion(H_func, t_grid, omega, order):
 
     return Lambda, F
 
-def exact_evolution(H_func, t_grid, omega, eval_index):
-    U = np.eye(H_func(0, omega).shape[0], dtype=complex)
+def exact_evolution(H_func, t_grid, omega, A, eval_index):
+    U = np.eye(H_func(0, omega, A).shape[0], dtype=complex)
     for i in range(1, eval_index+1):
         dt = t_grid[i] - t_grid[i-1]
-        U = expm(-1j * H_func(t_grid[i], omega) * dt) @ U
+        U = expm(-1j * H_func(t_grid[i], omega, A) * dt) @ U
     return U
 
 def FM_evolution(Lambda, F, order, t_grid, eval_index):
@@ -103,7 +103,7 @@ def evaluate_accuracy(U1, U2):
     fidelity = abs(np.trace(U1.conj().T @ U2))**2 / U1.shape[0]**2
     return fro_norm, fidelity
 
-def accuracy_plot(ax, Lambda, F, order, t_grid, omega):
+def accuracy_plot(ax, Lambda, F, order, t_grid, omega, A):
 
     #Frobenius vs Numerical U
     eval_indices = range(0, len(t_grid), 50)
@@ -111,29 +111,55 @@ def accuracy_plot(ax, Lambda, F, order, t_grid, omega):
     for o in orders_to_test:
         fros = []
         for idx in eval_indices:
-            U_exact = exact_evolution(H_func, t_grid, omega, idx)
+            U_exact = exact_evolution(H_func, t_grid, omega, A, idx)
             U_FM = FM_evolution(Lambda, F, o, t_grid, idx)
             fro, _ = evaluate_accuracy(U_FM, U_exact)
             fros.append(fro)
         ax.plot(t_grid[list(eval_indices)], fros, label=f"order={o}")
     ax.set_xlabel("t")
     ax.set_ylabel("Frobenius norm")
-    ax.set_title("Norm between Numerical and Floquet-Magnus Approximations of U")
+    ax.set_title("Norm between Numerical U and FME U")
     ax.legend()
     return
 
-def convergence_plot(ax, order, t_grid, omega):
-    freq_to_test = [1, 5, 10, 20]
+def convergence_plot(ax, order, t_grid, A):
+    freq_to_test = [0.1, 1, 5, 10, 20]
     for freq in freq_to_test:
         F_norm = []
-        _, F = floquet_magnus_expansion(H_func, t_grid, freq, order)
+        _, F = floquet_magnus_expansion(H_func, t_grid, freq, A, order)
         F_norm.extend([np.linalg.norm(F[n], 'fro') for n in range(order)])
-        ax.semilogy(list(range(order)), F_norm, label=f'frequency={freq}')
+        ax.semilogy(list(range(order)), F_norm, label=f'A/w={A/omega}')
     ax.set_xlabel("order")
     ax.set_ylabel("Norm of F")
     ax.set_title("Convergence of F for Varying Frequencies")
     ax.legend()
     return
+
+def micromotion_dev_plot(ax, Lambda, t_grid):
+    Lambda_sum = sum(Lambda[n] for n in range(order))
+    P = expm(-1j * Lambda_sum)
+    deviation = np.linalg.norm(P-np.eye(P.shape[1])[np.newaxis, :, :], axis=(-2,-1))
+    ax.plot(t_grid, deviation)
+    ax.set_xlabel("t")
+    ax.set_ylabel("Norm of P(t)-I")
+    ax.set_title("Deviation of Motion From the Effective Hamiltonian")
+
+
+def quasienergy_plot(ax, t_grid, omega):
+    amp_to_test = np.linspace(0.1, 5, 10)
+
+    quasienergy_array = []
+    for amp in amp_to_test:
+        _, F = floquet_magnus_expansion(H_func, t_grid, omega, amp, order)
+        quasienergies = np.linalg.eigvalsh(sum(F[n] for n in range(len(F))))
+        quasienergy_array.append(quasienergies)
+
+    quasienergy_array = np.array(quasienergy_array)
+    for n in range(len(quasienergy_array[0])):
+        ax.plot(amp_to_test/omega, quasienergy_array[:, n], 'o-', markersize=3)
+    ax.set_xlabel("A/w")
+    ax.set_ylabel("Quasienergy")
+    ax.set_title("Floquet Quasienergy Spectrum vs A/w")
 
 
 def commutator(A, B):
@@ -142,6 +168,7 @@ def commutator(A, B):
 if __name__ == "__main__":
     #Parameters
     omega = 5
+    A = 0.5
     period = 2 * np.pi/omega
     order = 20
     t_0 = 0
@@ -151,11 +178,15 @@ if __name__ == "__main__":
 
     #Approximation
     t_grid = np.linspace(0, period, N_t)
-    Lambda, F = floquet_magnus_expansion(H_func, t_grid, omega, order)
-    U_exact = exact_evolution(H_func, t_grid, omega, eval_index)
+    Lambda, F = floquet_magnus_expansion(H_func, t_grid, omega, A, order)
+    U_exact = exact_evolution(H_func, t_grid, omega, A, eval_index)
 
-    fig, axs = plt.subplots(2)
-    accuracy_plot(axs[0], Lambda, F, order, t_grid, omega)
-    convergence_plot(axs[1], order, t_grid, omega)
+    fig, axs = plt.subplots(2,2, figsize=(8,6))
+    accuracy_plot(axs[0,0], Lambda, F, order, t_grid, omega, A)
+    convergence_plot(axs[0,1], order, t_grid, A)
+    micromotion_dev_plot(axs[1,0], Lambda, t_grid)
+    quasienergy_plot(axs[1,1], t_grid, omega)
+    
     fig.tight_layout()
+    plt.savefig('Floquet-Magnus_Plots')
     plt.show()
