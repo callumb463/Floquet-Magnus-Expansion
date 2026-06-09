Fixed a sign error and added convergence and accuracy plots

Floquet-Magnus.py
@@ -6,7 +6,8 @@ from matplotlib.widgets import Button, Slider
 import customtkinter as ctk
 from scipy.integrate import cumulative_trapezoid
 
-def __main__():
+
+def main():
     ctk.set_appearance_mode("dark")  # "light" or "system"
     ctk.set_default_color_theme("blue")
 
@@ -21,7 +22,7 @@ def __main__():
 
     app.mainloop()
 
-def H_func(t):
+def H_func(t, omega):
     Sx = (1/np.sqrt(2)) * np.array([[0, 1, 0],
                                     [1, 0, 1],
                                     [0, 1, 0]], dtype=complex)
@@ -29,28 +30,28 @@ def H_func(t):
                    [0, 0, 0],
                    [0, 0,-1]], dtype=complex)
     # Static Sz + oscillating Sx — these don't commute
-    return Sz + 0.3 * np.sin(5*t) * Sx
+    return Sz + 0.5 * np.sin(omega*t) * Sx
 
 
-def floquet_magnus_expansion(H_func, t_grid, order):
-    dim = H_func(0).shape[0]
-    B = [float(bernoulli(n)/factorial(n)) for n in range(order + 1)]
+def floquet_magnus_expansion(H_func, t_grid, omega, order):
+    dim = H_func(0, omega).shape[0]
+    B = [float(bernoulli(n)/factorial(n)) for n in range(order)]
     H = np.zeros((len(t_grid), dim, dim), dtype=complex)
     for i, t in enumerate(t_grid):
-        H[i] = H_func(t)
+        H[i] = H_func(t, omega)
     F = np.empty(order, dtype=object)
-    G = np.empty(order+1, dtype=object)
-    W = np.empty((order+1, order+1), dtype=object)
-    T = np.empty((order+1, order+1), dtype=object)
+    G = np.empty(order, dtype=object)
+    W = np.empty((order, order), dtype=object)
+    T = np.empty((order, order), dtype=object)
     Lambda = np.empty(order, dtype=object)
     
 
     # Initialize F[0], G[0], T[0, 0], W[n, 0], and Lambda[0]
-    F[0] = np.mean(H, axis=0)
+    F[0] = F[0] = np.trapezoid(H, t_grid, axis=0) / period
     G[0] = H.copy()
     T[0, 0] = F[0]
     W[0, 0] = H.copy()
-    for i in range(order+1):
+    for i in range(1,order):
         W[i, 0] = np.zeros((N_t, dim, dim), dtype=complex)
     Lambda[0] = cumulative_trapezoid(H - F[0][np.newaxis, :, :], t_grid, axis=0, initial=0)
 
@@ -62,16 +63,16 @@ def floquet_magnus_expansion(H_func, t_grid, order):
                 assert T[n-m-1, j-1] is not None, f"T[{n-m-1},{j-1}] accessed but never set"
                 assert W[n-m-1, j-1] is not None, f"W[{n-m-1},{j-1}] accessed but never set"
             T[n-1, j] = np.sum(
-                [commutator(Lambda[m-1], T[n-m-1, j-1]) for m in range(1, n-j+1)],
+                [commutator(T[n-m-1, j-1], Lambda[m-1]) for m in range(1, n-j+1)],
                 axis=0
             )
             
             W[n-1, j] = np.sum(
-                [commutator(Lambda[m-1], W[n-m-1, j-1]) for m in range(1, n-j+1)],
+                [commutator(W[n-m-1, j-1], Lambda[m-1]) for m in range(1, n-j+1)],
                 axis=0
             )
         G[n-1] = sum([B[k] * ((-1j)**k) * (W[n-1, k] + ((-1)**(k+1)) * T[n-1, k]) for k in range(1, n)])
-        F[n-1] = np.mean(G[n-1], axis=0)
+        F[n-1] = np.trapezoid(G[n-1], t_grid, axis=0) / period
         T[n-1, 0] = F[n-1]
         Lambda[n-1] = cumulative_trapezoid(
             G[n-1] - F[n-1][np.newaxis, :, :], t_grid, axis=0, initial=0
@@ -79,16 +80,19 @@ def floquet_magnus_expansion(H_func, t_grid, order):
 
     return Lambda, F
 
-def exact_evolution(H_func, t_grid, eval_index):
-    U = np.eye(H_func(0).shape[0], dtype=complex)
+def exact_evolution(H_func, t_grid, omega, eval_index):
+    U = np.eye(H_func(0, omega).shape[0], dtype=complex)
     for i in range(1, eval_index+1):
         dt = t_grid[i] - t_grid[i-1]
-        U = expm(-1j * H_func(t_grid[i]) * dt) @ U
+        U = expm(-1j * H_func(t_grid[i], omega) * dt) @ U
     return U
 
-def FM_evolution(Lambda, order, F, t_grid, eval_index):
+def FM_evolution(Lambda, F, order, t_grid, eval_index):
     Lambda_sum = sum(Lambda[n] for n in range(order))
     F_sum = sum(F[n] for n in range(order))
+
+    #print(f"order={order}, Lambda_sum max={np.max(np.abs(Lambda_sum[eval_index])):.6f}, F_sum max={np.max(np.abs(F_sum)):.6f}")
+
     kick_op = expm(-1j * Lambda_sum[eval_index])
     floquet_op = expm(-1j * F_sum * t_grid[eval_index])
     U_FM = kick_op @ floquet_op
@@ -99,38 +103,59 @@ def evaluate_accuracy(U1, U2):
     fidelity = abs(np.trace(U1.conj().T @ U2))**2 / U1.shape[0]**2
     return fro_norm, fidelity
 
-def plot(Lambda, order):
-    fig, axes = plt.subplots(1, figsize=(8, 4))
+def accuracy_plot(ax, Lambda, F, order, t_grid, omega):
 
-    peaks = [np.max(np.abs(Lambda[n])) for n in range(order)]
-    semilog = axes.semilogy(range(order), peaks, 'o-')
-    axes.set_xlabel("Order")
-    axes.set_ylabel("Peak")
-    axes.set_title("Convergence of Lambda corrections")
+    #Frobenius vs Numerical U
+    eval_indices = range(0, len(t_grid), 50)
+    orders_to_test = [1, 2, 5, 10]
+    for o in orders_to_test:
+        fros = []
+        for idx in eval_indices:
+            U_exact = exact_evolution(H_func, t_grid, omega, idx)
+            U_FM = FM_evolution(Lambda, F, o, t_grid, idx)
+            fro, _ = evaluate_accuracy(U_FM, U_exact)
+            fros.append(fro)
+        ax.plot(t_grid[list(eval_indices)], fros, label=f"order={o}")
+    ax.set_xlabel("t")
+    ax.set_ylabel("Frobenius norm")
+    ax.set_title("Norm between Numerical and Floquet-Magnus Approximations of U")
+    ax.legend()
+    return
 
-    plt.tight_layout()
-    plt.show()
-    return fig, axes, semilog
+def convergence_plot(ax, order, t_grid, omega):
+    freq_to_test = [1, 5, 10, 20]
+    for freq in freq_to_test:
+        F_norm = []
+        _, F = floquet_magnus_expansion(H_func, t_grid, freq, order)
+        F_norm.extend([np.linalg.norm(F[n], 'fro') for n in range(order)])
+        ax.semilogy(list(range(order)), F_norm, label=f'frequency={freq}')
+    ax.set_xlabel("order")
+    ax.set_ylabel("Norm of F")
+    ax.set_title("Convergence of F for Varying Frequencies")
+    ax.legend()
+    return
+
 
 def commutator(A, B):
     return A @ B - B @ A
 
 if __name__ == "__main__":
     #Parameters
-    period = 2 * np.pi/5
-    order = 10
+    omega = 5
+    period = 2 * np.pi/omega
+    order = 20
     t_0 = 0
-    N_t = 1000
+    N_t = 5000
+    eval_index = 500
+
 
     #Approximation
     t_grid = np.linspace(0, period, N_t)
-    Lambda, F = floquet_magnus_expansion(H_func, t_grid, order)
-    U_exact = exact_evolution(H_func, t_grid, 400)
-    U_FM = FM_evolution(Lambda, order, F, t_grid, 400)
+    Lambda, F = floquet_magnus_expansion(H_func, t_grid, omega, order)
+    U_exact = exact_evolution(H_func, t_grid, omega, eval_index)
 
-    #Analysis
-    plot(Lambda, order)
-    fro_norm, fidelity = evaluate_accuracy(U_FM, U_exact)
-    print(f"Frobenius Norm: {fro_norm}")
-    print(f"Fidelity: {fidelity}") 
-    
+    fig, axs = plt.subplots(2)
+    accuracy_plot(axs[0], Lambda, F, order, t_grid, omega)
+    convergence_plot(axs[1], order, t_grid, omega)
+    fig.tight_layout()
+    plt.show()